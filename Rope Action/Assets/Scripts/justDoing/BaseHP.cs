3dd6740670eab7002aa70f13129a02ab88d5233b
20241103 이상훈 <162987094+waariioo@users.noreply.github.com>
2025-07-07 16:13:33 +0900

    BaseHP

--- Rope Action/Assets/Scripts/justDoing/BaseHP.cs
+++ Rope Action/Assets/Scripts/justDoing/BaseHP.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class BaseHP : MonoBehaviour
 {
+    [Header("체력 수치")]
+    [Tooltip("최대 체력")]
+    [SerializeField] protected float maxHP = 100f;
+
+    [Tooltip("현재 체력")]
+    [SerializeField] protected float curHP;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    virtual protected void Start()
     {
-        
+        FullHeal();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 현재 체력을 최대체력으로
+    /// </summary>
+    virtual protected void FullHeal()
     {
-        
+        if (maxHP > 0) { curHP = maxHP; }
+        else { Death(); }
+    }
+
+    /// <summary>
+    /// 데미지 입을 때 호출.
+    /// </summary>
+    /// <param name="damage">Damage변수 전달 요망.</param>
+    virtual public void Damage(Definition.Damage damage)
+    {
+        if (curHP - damage.AttackPower < 0)
+        {
+            Death();
+            return;
+        }
+
+        curHP -= damage.AttackPower;
+    }
+
+    /// <summary>
+    /// 현재 체력이 0미만일때 호출
+    /// </summary>
+    virtual protected void Death()
+    {
+
     }
 }
