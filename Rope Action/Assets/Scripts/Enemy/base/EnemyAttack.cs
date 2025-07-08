a501df90d4f373ee6b851bd6061645abf54ee361
20241103 이상훈 <162987094+waariioo@users.noreply.github.com>
2025-07-08 17:50:03 +0900

    oo

--- Rope Action/Assets/Scripts/Enemy/base/EnemyAttack.cs
+++ Rope Action/Assets/Scripts/Enemy/base/EnemyAttack.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class EnemyAttack : BaseAttack
 {
+    
+
     override public void Attack(BaseHP target, Damage damage)
     {
+        base.Attack(target, damage);
 
+        target.Damage(damage);
     }
 }
--- Rope Action/Assets/Scripts/Enemy/base/EnemyController.cs
+++ Rope Action/Assets/Scripts/Enemy/base/EnemyController.cs
@@ -1,16 +1,7 @@
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyController : BaseController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
